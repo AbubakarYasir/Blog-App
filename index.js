1db93b155d550032d4200b5f3877f75ad7c5521e
Abubakar Yasir <abubakaryasir569@gmail.com>
2022-03-24 16:03:47 +0500

    setting up App

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const port = process.env.PORT || 80;
+
+const app = express();
+app.use("/public", express.static(process.cwd() + "/public"));
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.listen(port, function () {
+    console.log("Server is started at http://localhost:" + port);
+});
