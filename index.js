025a52050246b20648ef3c41a5e6bc1b60703951
AbubakarYasir <abubakaryasir569@gmail.com>
2022-04-12 22:29:15 +0500

    finished

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const { request } = require("express");
-const lowerCase = require("lodash.lowercase");
+const _ = require("lodash");
 const posts = [];
 const homeStartingContent =
     "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -27,11 +27,16 @@ app.get("/", function (req, res) {
     });
 });
 app.get("/posts/:postName", function (req, res) {
-    const requestedTitle = req.params.postName;
+    const requestedTitle = _.lowerCase(req.params.postName);
     posts.forEach(function (post) {
-        const storedTitle = post.title;
+        const storedTitle = _.lowerCase(post.title);
         if (requestedTitle === storedTitle) {
-            console.log("Matched");
+            res.render("post", {
+                postTitle: post.title,
+                postBody: post.body,
+            });
+        } else {
+            res.send("404, Not Found");
         }
     });
 });
@@ -54,7 +59,7 @@ app.get("/compose", function (req, res) {
 app.post("/compose", function (req, res) {
     const post = {
         title: req.body.postTitle,
-        content: req.body.postBody,
+        body: req.body.postBody,
     };
     posts.push(post);
     res.redirect("/");
